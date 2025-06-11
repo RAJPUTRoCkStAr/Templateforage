1c115d58882b6a674ff822608c94a54c9ebc8060
Sumit Kumar Singh <115483120+RAJPUTRoCkStAr@users.noreply.github.com>
2025-06-11 12:56:01 +0530

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = "https://quzqlftqbigfchnlpdsu.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF1enFsZnRxYmlnZmNobmxwZHN1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgxMDEzOTcsImV4cCI6MjA2MzY3NzM5N30.6wFYcCdVkTSreOmVA1p2FQ1_uzEgT5RkBkgloJiwDKg";
+const supabaseUrl = "https";
+const supabaseAnonKey = "eyKg";
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error(
